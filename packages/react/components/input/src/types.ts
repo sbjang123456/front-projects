5ec4dfaa5162182fe4bbac2d3c8ed5f603f9b153
sbjang <sbjang123456@gmail.com>
2024-06-17 15:20:48 +0900

    feat: Input Group 및 Input left addons 컴포넌트 제작

--- packages/react/components/input/src/types.ts
+++ packages/react/components/input/src/types.ts
@@ -1,5 +1,6 @@
 import { UseInputProps } from "@sbjang/react-hooks-input";
 import { vars } from "@sbjang/themes";
+import { HTMLAttributes, ReactNode } from "react";
 
 type Size = "lg" | "md" | "sm" | "xs";
 type Color = keyof typeof vars.colors.$scale;
@@ -10,4 +11,16 @@ export type InputProps = UseInputProps & {
   variant?: "outline" | "filled";
   errorBorderColor?: string;
   focusBorderColor?: string;
+};
+
+export type InputGroupProps = {
+  color?: Color;
+  size?: Size;
+  children: ReactNode[];
+} & Omit<HTMLAttributes<HTMLDivElement>, "children">;
+
+export type InputLeftAddonProps = {
+  color?: Color;
+  size?: Size;
+  children: ReactNode;
 };
--- services/storybook/stories/ReactComponents/Input/Input.stories.tsx
+++ services/storybook/stories/ReactComponents/Input/Input.stories.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "@sbjang/react-components-input/style.css";
-import { Input } from "@sbjang/react-components-input";
+import {
+  Input,
+  InputGroup,
+  InputLeftAddon,
+} from "@sbjang/react-components-input";
 
 export default {
   title: "React Components/Input",
@@ -14,22 +18,22 @@ export const InputStory = {
   render: () => <Input placeholder="dd" />,
 };
 
-// export const InputGroupStory = {
-//   render: () => (
-//     <InputGroup size="lg" color="red">
-//       <InputLeftAddon>$</InputLeftAddon>
-//       <Input placeholder="dd" />
-//     </InputGroup>
-//   ),
-// };
+export const InputGroupStory = {
+  render: () => (
+    <InputGroup size="lg" color="red">
+      <InputLeftAddon>$</InputLeftAddon>
+      <Input placeholder="dd" />
+    </InputGroup>
+  ),
+};
 
-// export const InputVariantFilledStory = {
-//   render: () => <Input variant="filled" placeholder="dd" />,
-// };
+export const InputVariantFilledStory = {
+  render: () => <Input variant="filled" placeholder="dd" />,
+};
 
-// export const InputFocusVisibleState = {
-//   render: () => <Input />,
-//   parameters: {
-//     pseudo: { focusVisible: true },
-//   },
-// };
+export const InputFocusVisibleState = {
+  render: () => <Input />,
+  parameters: {
+    pseudo: { focusVisible: true },
+  },
+};
