9534dcce2bf60aaafbaec6b1082d6f508616eab5
sbjang <sbjang123456@gmail.com>
2024-06-20 09:37:35 +0900

    test: toast 컴포넌트 단위 테스트 작성

--- packages/react/components/toast/src/ToastContainer.tsx
+++ packages/react/components/toast/src/ToastContainer.tsx
@@ -3,7 +3,7 @@ import { toastContainerStyle } from "./style.css";
 
 export const ToastContainer = ({ children }: PropsWithChildren<{}>) => {
   return (
-    <div tabIndex={-1} className={toastContainerStyle}>
+    <div id="toast-container" tabIndex={-1} className={toastContainerStyle}>
       {children}
     </div>
   );
