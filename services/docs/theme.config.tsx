58237598551cfd3ac5a7cbe5136ba5b98eee8fe5
sbjang <sbjang123456@gmail.com>
2024-06-20 14:13:02 +0900

    fix(docs): theme config logo 텍스트로 수정

--- services/docs/theme.config.tsx
+++ services/docs/theme.config.tsx
@@ -1,7 +1,7 @@
 export default {
-    logo: <span>My Nextra Documentation</span>,
-    project: {
-      link: 'https://github.com/shuding/nextra'
-    }
-    // ... other theme options
-  }
+  logo: "My Nextra Documentation",
+  project: {
+    link: "https://github.com/shuding/nextra",
+  },
+  // ... other theme options
+};
